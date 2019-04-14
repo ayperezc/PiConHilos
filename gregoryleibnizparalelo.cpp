dbcb7713f2cd0f6ce9417fd46ee09fa0122a46eb
Yarid Perez <ayperezc@unal.edu.co>
2019-04-14 12:16:05 -0500

    hilos funcionales

--- gregoryleibnizparalelo.cpp
+++ gregoryleibnizparalelo.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <pthread.h>
 #include <iomanip>
+#include <ctime>
 
 using namespace std;
 
-#define NUM_THREADS 2 // 2,4,8,16
+#define NUM_THREADS 16 // 2,4,8,16
 
 struct entradas {
     double acum;
-    unsigned int inicio;
-    unsigned int fin;
+    long inicio;
+    long fin;
 };
 
 void gregLeibParaleloAntoguo(double &acum, long inicio, long fin){  //OJO!!!!!, esto da pi/4
@@ -28,56 +29,55 @@ void gregLeibParaleloAntoguo(double &acum, long inicio, long fin){  //OJO!!!!!, 
 void* gregLeibParalelo(void* args){  //OJO!!!!!, esto da pi/4
     struct entradas *misEntradas;
     misEntradas = (struct entradas *) args;
-    unsigned int inicio = misEntradas->inicio;
-    cout << inicio << endl;
-    unsigned int fin = misEntradas->fin;
-    cout << inicio << endl;
-    double acum = misEntradas->acum;
+    long inicio = misEntradas->inicio;
+    long fin = misEntradas->fin;
     double numerador,denominador,fraccion;
     int alternante = -1;
     
-    for(unsigned int i = inicio; i < fin; i++)
+    for(long i = inicio; i < fin; i++)
     {
         alternante = -alternante;
         numerador = alternante;
         denominador = 2*i + 1;
         fraccion = numerador/denominador;
-        acum = acum + fraccion;
+        misEntradas->acum = misEntradas->acum + fraccion;
     }
+
+    pthread_exit(NULL);
 }
-/*
+
 int main (void) {
     pthread_t threads[NUM_THREADS];
-    struct entradas td;
-    unsigned int paso = 1000000000/NUM_THREADS;
-    td.inicio = 0;
-    td.fin = 1000000000/NUM_THREADS;
+    struct entradas td[NUM_THREADS];
+    void *status;
+    long paso = 1000000000/NUM_THREADS;
     double acumTotal = 0;
-    td.acum = 0;
 
-    for (unsigned int i = 0 ; i < NUM_THREADS ; ++i)
+    clock_t t;
+    t = clock();
+
+    for (int i = 0; i < NUM_THREADS; ++i)
     {
-        td.acum = 0;
-        int t = pthread_create(&threads[i], NULL, gregLeibParalelo, (void *)&td);
- 
+        long imas1 = i+1;
+        td[i].inicio = i*paso;
+        td[i].fin = imas1*paso;
+        int t = pthread_create(&threads[i], NULL, gregLeibParalelo, (void *)&td[i]);
         if (t != 0)
         {
             cout << "Error in thread creation: " << t << endl;
         }
-        td.inicio = td.fin;
-        td.fin = td.fin + paso;
-        acumTotal += td.acum;
-        printf("Acumulado: %f", acumTotal);
     }
-    cout << std::setprecision(9) << 4*acumTotal << endl;
-}
-*/
-
-int main (void) {
-    struct entradas td;
-    td.inicio = 0;
-    td.fin = 10000;
-    td.acum = 0;
-    gregLeibParalelo((void *)&td);
-    printf("Acumulado: %f \n", td.acum);
+    for(int i = 0; i < NUM_THREADS; ++i)
+    {
+        int rc = pthread_join(threads[i], &status);
+        if (rc) {
+            cout << "Error:unable to join," << rc << endl;
+            exit(-1);
+        }
+        acumTotal = acumTotal + td[i].acum;
+    }
+    
+    cout << std::setprecision(15) << 4*acumTotal << endl;
+    t = clock() - t;
+    printf ("It took me %d clicks (%f seconds).\n",(int)t,((float)t)/CLOCKS_PER_SEC);
 }

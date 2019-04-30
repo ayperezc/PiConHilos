c1aff83064945c05bdec2828d7a34a99620f37f0
ayperezc <32652058+ayperezc@users.noreply.github.com>
2019-04-30 08:51:29 -0500

    particion del problema modificada

--- gregoryleibnizopenmp.cpp
+++ gregoryleibnizopenmp.cpp
@@ -4,51 +4,35 @@
 
 using namespace std;
 
-struct entradas {
-    double acum;
-    long inicio;
-    long fin;
-};
-
-void* gregLeibParalelo(void* args){  //OJO!!!!!, esto da pi/4
-    struct entradas *misEntradas;
-    misEntradas = (struct entradas *) args;
-    long inicio = misEntradas->inicio;
-    long fin = misEntradas->fin;
+double gregLeib(long i){
     double numerador,denominador,fraccion;
-    int alternante = -1;
+    int alternante = (i % 2 == 0)? 1 : -1 ;
+    double acum = 0;
     
-    for(long i = inicio; i < fin; i++)
-    {
-        alternante = -alternante;
-        numerador = alternante;
-        denominador = 2*i + 1;
-        fraccion = numerador/denominador;
-        misEntradas->acum = misEntradas->acum + fraccion;
-    }
+    numerador = alternante;
+    denominador = 2*i + 1;
+    fraccion = numerador/denominador;
+    return fraccion;
 }
 
 int main (void) {
-    struct entradas td[4];
     double acumTotal = 0;
-    long paso = 1000000000/4;
 
     clock_t t;
     t = clock();
 
-    #pragma omp parallel for
-    for (int i = 0; i < 4; ++i)
+    #pragma omp parallel for reduction(+:acumTotal)
+    for (long i = 0; i < 1000000000; ++i)
     {
-        long imas1 = i+1;
-        td[i].inicio = i*paso;
-        td[i].fin = imas1*paso;
-        gregLeibParalelo(&td[i]);
+	double acum = gregLeib(i);
+	acumTotal = acumTotal + acum;
     }
 
-    for(int i = 0; i < 4; ++i)
-    {
-        acumTotal = acumTotal + td[i].acum;
-    }
+
+    cout << std::setprecision(15) << 4*acumTotal << endl;
+    t = clock() - t;
+    printf ("It took me %d clicks (%f seconds).\n",(int)t,((float)t)/CLOCKS_PER_SEC);
+}
     
     cout << std::setprecision(15) << 4*acumTotal << endl;
     t = clock() - t;
